7ebad94ec9443d395cacad09e4918141318b5fe5
harshil05133 <rathodharshil05133@gmail.com>
2025-01-25 20:54:40 -0500

    changed some stuffs

--- react/src/pages/home.jsx
+++ react/src/pages/home.jsx
@@ -4,8 +4,10 @@ import './Home.css'
 function Home() {
     return (
         <div className='home-container'>
-            <h1>Welcome to the Volunteering Hub</h1>
+            <h1>Welcome to Guru Volunteering!</h1>
+            <hr></hr>
             <p>Your one-stop destination for all volunteering opportunities.</p>
+            <p>Learn what opportunities are near! Just enter your location and we'll do the searching for you!</p>
         </div>
     )
 }
